bfa6f94b25a82dd5ee6d2e043c498354eef76128
PonomareVlad <Git@PonomareVlad.ru>
2020-04-13 12:39:00 +0500

    Updated index page styles

--- index.php
+++ index.php
@@ -2,4 +2,40 @@
 require_once 'libs/db.lib.php';
 $data = mysqli_fetch_assoc(DB::select('Analytics', ['*'], 'duration="day"'));
 ?>
-<h1>За последний день было зафиксировано <?= $data['value'] ?> показов номеров</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Аналитика активности Драже</title>
+    <meta http-equiv="refresh" content="60">
+</head>
+<body>
+<h1><span><?= $data['value'] ?></span><br>показов номеров<br>были зафиксированы за последний день</h1>
+<style>
+    :root {
+        font-family: Helvetica, sans-serif;
+    }
+
+    body {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: unset;
+        padding: unset;
+    }
+
+    h1 {
+        text-align: center;
+        font-weight: 400;
+        color: gray;
+    }
+
+    h1 span {
+        color: black;
+        font-size: 2em;
+        font-weight: 300;
+    }
+</style>
+</body>
+</html>
