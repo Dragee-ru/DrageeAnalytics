80dee950f3c3462d64d00dd33fe933359cc4d580
PonomareVlad <Git@PonomareVlad.ru>
2020-04-13 12:47:46 +0500

    Updated index page

--- index.php
+++ index.php
@@ -7,22 +7,25 @@ $data = mysqli_fetch_assoc(DB::select('Analytics', ['*'], 'duration="day"'));
 <head>
     <meta charset="UTF-8">
     <title>Аналитика активности Драже</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="refresh" content="60">
 </head>
 <body>
-<h1><span><?= $data['value'] ?></span><br>показов номеров<br>были зафиксированы за последний день</h1>
+<h1><span><?= $data['value'] ?></span><br>количество показов номеров<br>зафиксированых за последний день</h1>
 <style>
     :root {
         font-family: Helvetica, sans-serif;
     }
 
     body {
+        box-sizing: border-box;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
         margin: unset;
-        padding: unset;
+        padding: 40px;
+        height: 100vh;
     }
 
     h1 {
